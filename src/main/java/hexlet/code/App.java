bce802103a35a01cf6237a411952548197d203c7
Vladislav.P <vladppk@protonmail.com>
2022-06-17 12:12:50 +0200

    Create help with picocli, add dependencies; 3rd step

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+
+@Command (
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        version = "0.0.1"
+)
+
+
 public class App {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean usageHelpRequest;
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionInfoRequest;
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        commandLine.execute(args);
+
     }
 }
