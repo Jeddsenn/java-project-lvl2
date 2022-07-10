43028e4966a7d7ca46ff8b595f294a2bdcc464ea
Vladislav.P <vladppk@protonmail.com>
2022-07-10 10:59:05 +0200

    refactoring

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -26,18 +26,19 @@ public class App implements Callable<Integer> {
     @Option(names = {"-f", "--format"}, defaultValue = "stylish", description = "output format", paramLabel = "format")
     private String format;
     @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
-    private Path filepath1;
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
 
-    public Path getFilepath1() {
+    public String getFilepath1() {
         return filepath1;
     }
 
-    public Path getFilepath2() {
+    public String getFilepath2() {
         return filepath2;
     }
 
-    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
-    private Path filepath2;
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -49,7 +50,7 @@ public class App implements Callable<Integer> {
     public Integer call() throws Exception {
 
 
-        System.out.println(generate(parseJsonToMap(String.valueOf(filepath1)), parseJsonToMap(String.valueOf(filepath2))));
+        System.out.println(generate(filepath1, filepath2));
         return 0;
     }
 }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -7,9 +7,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import static hexlet.code.Parser.parseJsonToMap;
+
 public class Differ {
 
-    public static String generate(Map<String, Object> firstMap, Map<String, Object> secondMap) {
+/*    public static String generate(Map<String, Object> firstMap, Map<String, Object> secondMap) {
         List<KeyDifference> result = new ArrayList<>();
 
         for (String key : firstMap.keySet()) {
@@ -35,7 +37,54 @@ public class Differ {
         }
 
         result.sort(Comparator.comparing(KeyDifference::getKeyValue));
+        System.out.println(result);
+        StringBuilder resultString = new StringBuilder("{\n  ");
+        for (KeyDifference diff : result) {
+            resultString.append(diff.formatter());
+        }
 
+        resultString.deleteCharAt(resultString.length() - 1);
+        resultString.deleteCharAt(resultString.length() - 1);
+        resultString.append("}");
+        return resultString.toString();
+    }*/
+
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        var a = parseJsonToMap(String.valueOf(filepath1));
+        var b = parseJsonToMap(String.valueOf(filepath2));
+        var c = generateList(a,b);
+        var d = generateString(c);
+        return d;
+    }
+    public static List<KeyDifference> generateList(Map<String, Object> firstMap, Map<String, Object> secondMap) {
+        List<KeyDifference> result = new ArrayList<>();
+
+        for (String key : firstMap.keySet()) {
+            KeyDifference tmp = new KeyDifference();
+            tmp.setKeyValue(key);
+            tmp.setInFirstJson(true);
+            tmp.setFirstValue(String.valueOf(firstMap.get(key)));
+            if (secondMap.containsKey(key)) {
+                tmp.setInSecondJson(true);
+                tmp.setSecondValue(String.valueOf(secondMap.get(key)));
+            }
+            result.add(tmp);
+        }
+
+        for (String key : secondMap.keySet()) {
+            if (!firstMap.containsKey(key)) {
+                KeyDifference tempObj = new KeyDifference();
+                tempObj.setKeyValue(key);
+                tempObj.setInSecondJson(true);
+                tempObj.setSecondValue(String.valueOf(secondMap.get(key)));
+                result.add(tempObj);
+            }
+        }
+        result.sort(Comparator.comparing(KeyDifference::getKeyValue));
+        return result;
+    }
+
+    public static String generateString(List<KeyDifference> result) {
         StringBuilder resultString = new StringBuilder("{\n  ");
         for (KeyDifference diff : result) {
             resultString.append(diff.formatter());
