32dac277eecaeb9dd809eb8d44f76ee59fe9b240
Vladislav.P <vladppk@protonmail.com>
2022-07-10 13:42:51 +0200

    fix tests

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -1,76 +1,56 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
+import static hexlet.code.Differ.generate;
 import static hexlet.code.Parser.generatePathToFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
 
     @Test
-    void testGenerate() throws IOException {
+    void testGenerate() throws Exception {
+        String format = "stylish";
         String path1 = "src/test/resources/file1.json";
         String path2 = "src/test/resources/file2.json";
         String path3 = "src/test/resources/file12.json";
         String fileThirdJson = generatePathToFile(path3);
-
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> mapFirst = mapper.readValue(generatePathToFile(path1), new TypeReference<>() {
-        });
-        Map<String, Object> mapSecond = mapper.readValue(generatePathToFile(path2), new TypeReference<>() {
-        });
-        assertEquals(fileThirdJson, new Differ().generate(mapFirst, mapSecond));
+        String output = generate(format, path1, path2);
+        assertEquals(fileThirdJson, output);
     }
 
     @Test
-    void testGenerateYaml() throws IOException {
+    void testGenerateYaml() throws Exception {
+        String format = "stylish";
         String path1 = "src/test/resources/file1.yaml";
         String path2 = "src/test/resources/file2.yaml";
         String path3 = "src/test/resources/file12.json";
         String fileThirdJson = generatePathToFile(path3);
 
-
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> mapFirst = mapper.readValue(generatePathToFile(path1), new TypeReference<>() {
-        });
-        Map<String, Object> mapSecond = mapper.readValue(generatePathToFile(path2), new TypeReference<>() {
-        });
-        assertEquals(fileThirdJson, new Differ().generate(mapFirst, mapSecond));
+        String output = generate(format, path1, path2);
+        assertEquals(fileThirdJson, output);
     }
 
     @Test
-    void testGenerateNestedJson() throws IOException {
+    void testGenerateNestedJson() throws Exception {
+        String format = "stylish";
         String path1 = "src/test/resources/file31.json";
         String path2 = "src/test/resources/file32.json";
         String path3 = "src/test/resources/file34.json";
         String fileThirdJson = generatePathToFile(path3);
 
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> mapFirst = mapper.readValue(generatePathToFile(path1), new TypeReference<>() {
-        });
-        Map<String, Object> mapSecond = mapper.readValue(generatePathToFile(path2), new TypeReference<>() {
-        });
-        assertEquals(fileThirdJson, new Differ().generate(mapFirst, mapSecond));
+        String output = generate(format, path1, path2);
+        assertEquals(fileThirdJson, output);
     }
 
     @Test
-    void testGenerateNestedYaml() throws IOException {
+    void testGenerateNestedYaml() throws Exception {
+        String format = "stylish";
         String path1 = "src/test/resources/file41.yaml";
         String path2 = "src/test/resources/file42.yaml";
         String path3 = "src/test/resources/file34.json";
         String fileThirdJson = generatePathToFile(path3);
 
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> mapFirst = mapper.readValue(generatePathToFile(path1), new TypeReference<>() {
-        });
-        Map<String, Object> mapSecond = mapper.readValue(generatePathToFile(path2), new TypeReference<>() {
-        });
-        assertEquals(fileThirdJson, new Differ().generate(mapFirst, mapSecond));
+        String output = generate(format, path1, path2);
+        assertEquals(fileThirdJson, output);
     }
 }
