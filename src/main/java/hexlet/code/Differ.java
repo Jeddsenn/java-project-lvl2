ac8dca910396a1b597938f3e8ec293dad5961194
Vladislav.P <vladppk@protonmail.com>
2022-07-13 12:13:19 +0200

    a bit of refactoring

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,8 @@
 package hexlet.code;
 
-import hexlet.code.ComparedData.KeyDifference;
-
 import java.util.*;
-
+import static hexlet.code.ComparedData.generateListOfDifferences;
+import static hexlet.code.Formatters.Formatter.generateJsonOutput;
 import static hexlet.code.Parser.parseJsonToMap;
 
 public class Differ {
@@ -11,13 +10,20 @@ public class Differ {
     public static String generate(String format, String filepath1, String filepath2) throws Exception {
         Map<String, Object> firstMap = parseJsonToMap(String.valueOf(filepath1));
         Map<String, Object> secondMap = parseJsonToMap(String.valueOf(filepath2));
-        List<KeyDifference> listOfDifference = ComparedData.generateList(firstMap,secondMap);
+        List<Map<String, Object>> listOfDifference = generateListOfDifferences(firstMap,secondMap);
+
         String formattedOutput = "";
+//        String formattedOutput = new Formatter(format);
 
         if (Objects.equals(format, "stylish")) {
-            formattedOutput = Formatter.generateStylishOutput(listOfDifference);
+            formattedOutput = generateJsonOutput(listOfDifference);
+        }
+        else if (Objects.equals(format, "plain")){
+            formattedOutput = generateJsonOutput(listOfDifference);
+        } else if (Objects.equals(format, "json")) {
+            formattedOutput = generateJsonOutput(listOfDifference);
+
         }
         return formattedOutput;
     }
-
 }
