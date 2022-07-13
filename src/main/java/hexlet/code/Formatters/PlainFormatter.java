bf7163e50498970c607ec886848f8ba71351b5c5
Vladislav.P <vladppk@protonmail.com>
2022-07-13 22:30:23 +0200

    add PlainFormatter

--- src/main/java/hexlet/code/Formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/Formatters/PlainFormatter.java
@@ -1,11 +1,62 @@
 package hexlet.code.Formatters;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PlainFormatter {
-    public static String generatePlainOutput(List<Map<String, Object>> listOfDifference) {
-        String output = "";
-        return output;
+    public static String generatePlainOutput(List<Map<String, Object>> listOfDifferences) {
+        StringBuilder result = new StringBuilder("");
+        for (Map<String, Object> l : listOfDifferences) {
+            var status = l.get("status");
+            var v = l.get("value");
+            var v1 = l.get("value1");
+            var v2 = l.get("value2");
+            var k = l.get("key");
+            if (v instanceof List<?> || v instanceof Map<?,?>) {
+                v = "[complex value]";
+            }
+            if (v1 instanceof List<?> || v1 instanceof Map<?,?>) {
+                v1 = "[complex value]";
+            }
+            if (v2 instanceof List<?> || v2 instanceof Map<?,?>) {
+                v2 = "[complex value]";
+            }
+
+            if (status.equals("deleted")) {                      //removed
+                    result.append("Property ")
+                            .append("'")
+                            .append(k)
+                            .append("'")
+                            .append(" was removed")
+                            .append("\n");
+            }
+          if (status.equals("added")) {                                      //added
+                result.append("Property ")
+                        .append("'")
+                        .append(k)
+                        .append("'")
+                        .append(" was added with value: ")
+                        .append(v)
+                        .append("\n");
+            }
+          if (status.equals("same")) {
+             result.append("");
+           } else if (status.equals("changed")) {                     //updated
+                result.append("Property ")
+                        .append("'")
+                        .append(k)
+                        .append("'")
+                        .append(" was updated. From ")
+                        .append("'")
+                        .append(v1)
+                        .append("'")
+                        .append(" to ")
+                        .append("'")
+                        .append(v2)
+                        .append("'")
+                        .append("\n");
+            }
+        }
+        result.trimToSize();
+        return result.toString();
     }
 }
