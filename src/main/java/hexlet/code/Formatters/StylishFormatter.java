0135cf87a4bfd683b59f96077c432836d3a304e4
Vladislav.P <vladppk@protonmail.com>
2022-07-13 19:19:18 +0200

    add stylish Formatter

--- src/main/java/hexlet/code/Formatters/StylishFormatter.java
+++ src/main/java/hexlet/code/Formatters/StylishFormatter.java
@@ -4,43 +4,29 @@ import hexlet.code.ComparedData;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class StylishFormatter {
 
     public static String generateStylishOutput(List<Map<String, Object>> listOfDifferences) {
-        String result = "";
-/*
-        StringBuilder resultString = new StringBuilder("{\n  ");
-        for (ComparedData.KeyDifference diff : result) {
-            resultString.append(formatter(diff));
+        StringBuilder result = new StringBuilder("{\n");
+        for (Map<String, Object> l : listOfDifferences) {
+            var status = l.get("status");
+            if (status.equals("deleted")) {
+                result.append("  - ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
+            }
+            if (status.equals("added")) {
+                result.append("  + ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
+            } else if (status.equals("same")) {
+                result.append("    ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
+            } else if (status.equals("changed")) {
+                result.append("  - ").append(l.get("key")).append(": ").append(l.get("value1")).append("\n");
+                result.append("  + ").append(l.get("key")).append(": ").append(l.get("value2")).append("\n");
+            }
         }
-        resultString.deleteCharAt(resultString.length() - 1);
-        resultString.deleteCharAt(resultString.length() - 1);
-        resultString.append("}");
-        return resultString.toString();
-    }
-    public static String toStylishFormat(String result) {
-        String r = "";
-
-*/
-
-        return result;
+        result.append("}");
+        return result.toString();
     }
 }
 
 
-/*
-Property 'chars2' was updated. From [complex value] to false
-        Property 'checked' was updated. From false to true
-        Property 'default' was updated. From null to [complex value]
-        Property 'id' was updated. From 45 to null
-        Property 'key1' was removed
-        Property 'key2' was added with value: 'value2'
-        Property 'numbers2' was updated. From [complex value] to [complex value]
-        Property 'numbers3' was removed
-        Property 'numbers4' was added with value: [complex value]
-        Property 'obj1' was added with value: [complex value]
-        Property 'setting1' was updated. From 'Some value' to 'Another value'
-        Property 'setting2' was updated. From 200 to 300
-        Property 'setting3' was updated. From true to 'none'
-*/
