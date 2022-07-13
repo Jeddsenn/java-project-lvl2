d121ef64d1f295ce5661d4c25fc768282f55fe85
Vladislav.P <vladppk@protonmail.com>
2022-07-13 14:51:44 +0200

    moved formatters logic out of Differ

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,9 @@
 package hexlet.code;
 
+import hexlet.code.Formatters.Formatter;
+
 import java.util.*;
 import static hexlet.code.ComparedData.generateListOfDifferences;
-import static hexlet.code.Formatters.Formatter.generateJsonOutput;
 import static hexlet.code.Parser.parseJsonToMap;
 
 public class Differ {
@@ -11,19 +12,6 @@ public class Differ {
         Map<String, Object> firstMap = parseJsonToMap(String.valueOf(filepath1));
         Map<String, Object> secondMap = parseJsonToMap(String.valueOf(filepath2));
         List<Map<String, Object>> listOfDifference = generateListOfDifferences(firstMap,secondMap);
-
-        String formattedOutput = "";
-//        String formattedOutput = new Formatter(format);
-
-        if (Objects.equals(format, "stylish")) {
-            formattedOutput = generateJsonOutput(listOfDifference);
-        }
-        else if (Objects.equals(format, "plain")){
-            formattedOutput = generateJsonOutput(listOfDifference);
-        } else if (Objects.equals(format, "json")) {
-            formattedOutput = generateJsonOutput(listOfDifference);
-
-        }
-        return formattedOutput;
+        return Formatter.format(format, listOfDifference);
     }
 }
