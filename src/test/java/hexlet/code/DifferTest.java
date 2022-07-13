edfce0992c0bce3bd4e7e74a3881aa6b7de1f96e
Vladislav.P <vladppk@protonmail.com>
2022-07-13 19:41:16 +0200

    update tests according to method signature

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -8,30 +8,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DifferTest {
 
     @Test
-    void testGenerate() throws Exception {
+    void testGenerateStylishJson() throws Exception {
         String format = "stylish";
         String path1 = "src/test/resources/file1.json";
         String path2 = "src/test/resources/file2.json";
         String path3 = "src/test/resources/file12.json";
         String fileThirdJson = generatePathToFile(path3);
-        String output = generate(format, path1, path2);
+        String output = generate("stylish", path1, path2);
         assertEquals(fileThirdJson, output);
     }
 
     @Test
-    void testGenerateYaml() throws Exception {
+    void testGenerateStylishYaml() throws Exception {
         String format = "stylish";
         String path1 = "src/test/resources/file1.yaml";
         String path2 = "src/test/resources/file2.yaml";
         String path3 = "src/test/resources/file12.json";
         String fileThirdJson = generatePathToFile(path3);
-
-        String output = generate(format, path1, path2);
+        String output = generate("stylish", path1, path2);
         assertEquals(fileThirdJson, output);
     }
 
     @Test
-    void testGenerateNestedJson() throws Exception {
+    void testGenerateNestedStylishJson() throws Exception {
         String format = "stylish";
         String path1 = "src/test/resources/file31.json";
         String path2 = "src/test/resources/file32.json";
@@ -43,7 +42,7 @@ class DifferTest {
     }
 
     @Test
-    void testGenerateNestedYaml() throws Exception {
+    void testGenerateNestedStylishYaml() throws Exception {
         String format = "stylish";
         String path1 = "src/test/resources/file41.yaml";
         String path2 = "src/test/resources/file42.yaml";
