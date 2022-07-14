52a03331c9e2a02e6900b4a71f51ec540292dcca
Vladislav.P <vladppk@protonmail.com>
2022-07-14 18:13:52 +0200

    update PlainFormatter

--- src/main/java/hexlet/code/Formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/Formatters/PlainFormatter.java
@@ -1,35 +1,28 @@
 package hexlet.code.Formatters;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class PlainFormatter {
     public static String generatePlainOutput(List<Map<String, Object>> listOfDifferences) {
         StringBuilder result = new StringBuilder("");
         for (Map<String, Object> l : listOfDifferences) {
             var status = l.get("status");
-            var v = l.get("value");
-            var v1 = l.get("value1");
-            var v2 = l.get("value2");
+            var v = modifyOutputOfComplexObject(addStringSymbol(l.get("value")));
+            var v1 = modifyOutputOfComplexObject(addStringSymbol(l.get("value1")));
+            var v2 = modifyOutputOfComplexObject(addStringSymbol(l.get("value2")));
             var k = l.get("key");
-            if (v instanceof List<?> || v instanceof Map<?,?>) {
-                v = "[complex value]";
-            }
-            if (v1 instanceof List<?> || v1 instanceof Map<?,?>) {
-                v1 = "[complex value]";
-            }
-            if (v2 instanceof List<?> || v2 instanceof Map<?,?>) {
-                v2 = "[complex value]";
-            }
 
             if (status.equals("deleted")) {                      //removed
-                    result.append("Property ")
-                            .append("'")
-                            .append(k)
-                            .append("'")
-                            .append(" was removed")
-                            .append("\n");
+                result.append("Property ")
+                        .append("'")
+                        .append(k)
+                        .append("'")
+                        .append(" was removed")
+                        .append("\n");
             }
-          if (status.equals("added")) {                                      //added
+            if (status.equals("added")) {                                      //added
                 result.append("Property ")
                         .append("'")
                         .append(k)
@@ -38,25 +31,34 @@ public class PlainFormatter {
                         .append(v)
                         .append("\n");
             }
-          if (status.equals("same")) {
-             result.append("");
-           } else if (status.equals("changed")) {                     //updated
+            if (status.equals("same")) {
+                result.append("");
+            } else if (status.equals("changed")) {                     //updated
                 result.append("Property ")
                         .append("'")
                         .append(k)
                         .append("'")
                         .append(" was updated. From ")
-                        .append("'")
                         .append(v1)
-                        .append("'")
                         .append(" to ")
-                        .append("'")
                         .append(v2)
-                        .append("'")
                         .append("\n");
             }
         }
         result.trimToSize();
         return result.toString();
+    }
+
+    private static Object modifyOutputOfComplexObject(Object v) {
+        if (v instanceof Collection || v instanceof Map<?, ?>) {
+            return (String) "[complex value]";
+        }
+        return v;
+    }
+    static Object addStringSymbol(Object v) {
+        if (v instanceof String) {
+            return "\'" + v + "\'";
+        }
+        return v;
     }
 }
