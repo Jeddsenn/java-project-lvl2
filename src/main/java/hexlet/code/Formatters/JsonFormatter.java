07a50b8f4357d4d3dfcf1f40471ddf1cc9f9592a
Vladislav.P <vladppk@protonmail.com>
2022-07-15 17:27:22 +0200

    fix JsonFormatter

--- src/main/java/hexlet/code/Formatters/JsonFormatter.java
+++ src/main/java/hexlet/code/Formatters/JsonFormatter.java
@@ -10,20 +10,16 @@ public class JsonFormatter {
     public static String generateJsonOutput(List<Map<String, Object>> listOfDifferences)
             throws JsonProcessingException {
         StringBuilder sb = new StringBuilder("");
-        ObjectMapper objectMapper = new ObjectMapper();
-        for (Map<String, Object> list : listOfDifferences) {
-            sb.append((objectMapper.writeValueAsString(list)));
-        }
-        return toJsonFormat(sb.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(listOfDifferences);
+        return toJsonFormat(result);
     }
 
     private static String toJsonFormat(String result) {
-        return result.replace(":", ": ")
-                .replace(",", ",\n")
-                .replace("[{", "[  \n{")
-                .replace("}, {", "}\n,\n{")
-                .replace("}{", "}\n,\n{")
-                .replace("}]", "\n}\n]");
+        return result.replace(" : ", ": ")
+                .replace("[ {", "[\n  {")
+                .replace("}, {", "  },\n  {")
+                .replace("  \"", "    \"")
+                .replace("} ]", "  }\n]");
     }
 }
-
