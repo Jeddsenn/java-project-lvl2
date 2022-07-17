87b09abad38c8ba05ad529a946fe17b3a1f5caab
Vladislav.P <vladppk@protonmail.com>
2022-07-17 15:59:39 +0200

    rename method as more understandable

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -10,13 +10,13 @@ import static hexlet.code.Parser.parseJsonToMap;
 
 public class Differ {
 
-    public static String generate(String filePath1, String filePath2, String format) throws Exception {
+    public static String generateDiff(String filePath1, String filePath2, String format) throws Exception {
         Map<String, Object> firstMap = parseJsonToMap(String.valueOf(filePath1));
         Map<String, Object> secondMap = parseJsonToMap(String.valueOf(filePath2));
         List<Map<String, Object>> listOfDifference = generateListOfDifferences(firstMap, secondMap);
         return Formatter.format(format, listOfDifference);
     }
-    public static String generate(String filePath1, String filePath2) throws Exception {
-        return generate(filePath1, filePath2, "stylish");
+    public static String generateDiff(String filePath1, String filePath2) throws Exception {
+        return generateDiff(filePath1, filePath2, "stylish");
     }
 }
