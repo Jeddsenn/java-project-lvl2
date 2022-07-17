6ea318adb1e9be2bb15d2c67a5905a0c2203e468
Vladislav.P <vladppk@protonmail.com>
2022-07-17 15:37:51 +0200

    refactor StylishFormatter

--- src/main/java/hexlet/code/Formatters/StylishFormatter.java
+++ src/main/java/hexlet/code/Formatters/StylishFormatter.java
@@ -4,21 +4,34 @@ import java.util.List;
 import java.util.Map;
 
 public class StylishFormatter {
+    private static final String STATUS = "status";
+    private static final String DELETED = "deleted";
+    private static final String ADDED = "added";
+    private static final String SAME = "same";
+    private static final String CHANGED = "changed";
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+    private static final String VALUE1 = "value1";
+    private static final String VALUE2 = "value2";
+    private static final String MINUS = "  - ";
+    private static final String PLUS  = "  + ";
+    private static final String SPACE = "    ";
+    private static final String COLON = ": ";
 
     public static String generateStylishOutput(List<Map<String, Object>> listOfDifferences) {
         StringBuilder result = new StringBuilder("{\n");
         for (Map<String, Object> l : listOfDifferences) {
-            var status = l.get("status");
-            if (status.equals("deleted")) {
-                result.append("  - ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
+            var status = l.get(STATUS);
+            if (status.equals(DELETED)) {
+                result.append("  - ").append(l.get(KEY)).append(COLON).append(l.get(VALUE)).append("\n");
             }
-            if (status.equals("added")) {
-                result.append("  + ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
-            } else if (status.equals("same")) {
-                result.append("    ").append(l.get("key")).append(": ").append(l.get("value")).append("\n");
-            } else if (status.equals("changed")) {
-                result.append("  - ").append(l.get("key")).append(": ").append(l.get("value1")).append("\n");
-                result.append("  + ").append(l.get("key")).append(": ").append(l.get("value2")).append("\n");
+            if (status.equals(ADDED)) {
+                result.append(PLUS).append(l.get(KEY)).append(COLON).append(l.get(VALUE)).append("\n");
+            } else if (status.equals(SAME)) {
+                result.append(SPACE).append(l.get(KEY)).append(COLON).append(l.get(VALUE)).append("\n");
+            } else if (status.equals(CHANGED)) {
+                result.append(MINUS).append(l.get(KEY)).append(COLON).append(l.get(VALUE1)).append("\n");
+                result.append(PLUS).append(l.get(KEY)).append(COLON).append(l.get(VALUE2)).append("\n");
             }
         }
         result.append("}");
