0410c951e35f5241efe6e1ed085decc0d8adcf21
Vladislav.P <vladppk@protonmail.com>
2022-07-17 15:52:37 +0200

    refactor PlainFormatter, add static final String variables, delete redundant condition, downcast etc

--- src/main/java/hexlet/code/Formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/Formatters/PlainFormatter.java
@@ -5,43 +5,45 @@ import java.util.List;
 import java.util.Map;
 
 public class PlainFormatter {
-    public static String generatePlainOutput(List<Map<String, Object>> listOfDifferences) {
-        StringBuilder result = new StringBuilder("");
-        for (Map<String, Object> l : listOfDifferences) {
-            var status = l.get("status");
-            var v = modifyOutputOfComplexObject(addStringSymbol(l.get("value")));
-            var v1 = modifyOutputOfComplexObject(addStringSymbol(l.get("value1")));
-            var v2 = modifyOutputOfComplexObject(addStringSymbol(l.get("value2")));
-            var k = l.get("key");
 
-            if (status.equals("deleted")) {                      //removed
-                result.append("Property ")
-                        .append("'")
-                        .append(k)
-                        .append("'")
+    private static final String STATUS = "status";
+    private static final String DELETED = "deleted";
+    private static final String ADDED = "added";
+    private static final String CHANGED = "changed";
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+    private static final String VALUE1 = "value1";
+    private static final String VALUE2 = "value2";
+    private static final String PROPERTY = "Property ";
+    public static String generatePlainOutput(List<Map<String, Object>> listOfDifferences) {
+        StringBuilder result = new StringBuilder();
+        for (Map<String, Object> l : listOfDifferences) {
+            var status = l.get(STATUS);
+            var value = modifyOutputOfComplexObject(addStringSymbol(l.get(VALUE)));
+            var value1 = modifyOutputOfComplexObject(addStringSymbol(l.get(VALUE1)));
+            var value2 = modifyOutputOfComplexObject(addStringSymbol(l.get(VALUE2)));
+            var key = addStringSymbol(l.get(KEY));
+
+            if (status.equals(DELETED)) {                      //removed
+                result.append(PROPERTY)
+                        .append(key)
                         .append(" was removed")
                         .append("\n");
             }
-            if (status.equals("added")) {                                      //added
-                result.append("Property ")
-                        .append("'")
-                        .append(k)
-                        .append("'")
+            if (status.equals(ADDED)) {                                      //added
+                result.append(PROPERTY)
+                        .append(key)
                         .append(" was added with value: ")
-                        .append(v)
+                        .append(value)
                         .append("\n");
             }
-            if (status.equals("same")) {
-                result.append("");
-            } else if (status.equals("changed")) {                     //updated
-                result.append("Property ")
-                        .append("'")
-                        .append(k)
-                        .append("'")
+            else if (status.equals(CHANGED)) {                     //updated
+                result.append(PROPERTY)
+                        .append(key)
                         .append(" was updated. From ")
-                        .append(v1)
+                        .append(value1)
                         .append(" to ")
-                        .append(v2)
+                        .append(value2)
                         .append("\n");
             }
         }
@@ -51,14 +53,14 @@ public class PlainFormatter {
 
     private static Object modifyOutputOfComplexObject(Object v) {
         if (v instanceof Collection || v instanceof Map<?, ?>) {
-            return (String) "[complex value]";
+            return "[complex value]";
         }
         return v;
     }
-    static Object addStringSymbol(Object v) {
-        if (v instanceof String) {
-            return "\'" + v + "\'";
+    static Object addStringSymbol(Object value) {
+        if (value instanceof String) {
+            return "'" + value + "'";
         }
-        return v;
+        return value;
     }
 }
