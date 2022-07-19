c03c43754b97924ae82607cac8512783b11e1844
Vladislav.P <vladppk@protonmail.com>
2022-07-19 21:51:33 +0200

    add catching and throwing Exceptions

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -18,13 +18,17 @@ import java.util.concurrent.Callable;
 public final class App implements Callable<Integer> {
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean usageHelpRequest;
+
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     private boolean versionInfoRequest;
+
     @Option(names = {"-f", "--format"}, defaultValue = "stylish",
             description = "the file to use (default: ${DEFAULT-VALUE})", paramLabel = "format")
     private String format;
+
     @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
     private String filepath1;
+
     @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
     private String filepath2;
 
@@ -36,7 +40,12 @@ public final class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println(Differ.generate(filepath1, filepath2, format));
+        try {
+            System.out.println(Differ.generate(filepath1, filepath2, format));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("1"); //return 1; ?
+        }
         return 0;
     }
 }
