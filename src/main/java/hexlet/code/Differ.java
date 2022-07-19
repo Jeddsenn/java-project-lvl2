15ad87c1c302334c07fbf7355e9fbafba56764fa
Vladislav.P <vladppk@protonmail.com>
2022-07-19 21:50:45 +0200

    refactor generate method, add getData metod, move in getFileExtension and generatePathToFile methods

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,22 +1,37 @@
 package hexlet.code;
 
-import hexlet.code.Formatters.Formatter;
+import hexlet.code.formatters.Formatter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
-import static hexlet.code.ComparedData.generateListOfDifferences;
-import static hexlet.code.Parser.parseJsonToMap;
 
 public class Differ {
 
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> firstMap = parseJsonToMap(String.valueOf(filePath1));
-        Map<String, Object> secondMap = parseJsonToMap(String.valueOf(filePath2));
-        List<Map<String, Object>> listOfDifference = generateListOfDifferences(firstMap, secondMap);
+        Map<String, Object> firstMap1 = getData(String.valueOf(filePath1));
+        Map<String, Object> secondMap2 = getData(String.valueOf(filePath2));
+        List<Map<String, Object>> listOfDifference = Tree.build(firstMap1, secondMap2);
         return Formatter.format(format, listOfDifference);
     }
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
+    }
+    public static Map<String, Object> getData(String filePath) throws Exception {
+        String path = generatePathToFile(filePath);
+        String extension = getFileExtension(filePath);
+        return Parser.dataStructureToMap(path, extension);
+    }
+
+    public static String getFileExtension(String fileName) {
+        int lastDot = fileName.lastIndexOf('.');
+        return fileName.substring(lastDot + 1);
+    }
+
+    public static String generatePathToFile(String fileName) throws IOException {
+        return Files.readString(Paths.get(fileName).toAbsolutePath().normalize());
     }
 }
