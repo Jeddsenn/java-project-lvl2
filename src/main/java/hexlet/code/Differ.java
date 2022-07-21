f081eb17a5f258bafd612a370a75bd4516da5161
Vladislav.P <vladppk@protonmail.com>
2022-07-21 17:28:42 +0200

    variable typo fix

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import hexlet.code.formatters.Formatter;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,14 +11,16 @@ import java.util.Map;
 public class Differ {
 
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> firstMap1 = getData(String.valueOf(filePath1));
-        Map<String, Object> secondMap2 = getData(String.valueOf(filePath2));
-        List<Map<String, Object>> listOfDifference = Tree.build(firstMap1, secondMap2);
+        Map<String, Object> data1 = getData(filePath1);
+        Map<String, Object> data2 = getData(filePath2);
+        List<Map<String, Object>> listOfDifference = Tree.build(data1, data2);
         return Formatter.format(format, listOfDifference);
     }
+
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
     }
+
     public static Map<String, Object> getData(String filePath) throws Exception {
         String path = generatePathToFile(filePath);
         String extension = getFileExtension(filePath);
@@ -34,4 +35,5 @@ public class Differ {
     public static String generatePathToFile(String fileName) throws IOException {
         return Files.readString(Paths.get(fileName).toAbsolutePath().normalize());
     }
+
 }
